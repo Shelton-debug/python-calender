d64f3ee44d2734028f0ec260bec7e2a4db4bcf85
Shelton-debug <sheltpnotieno47@gmail.com>
2025-09-13 21:06:31 +0300

    calender

--- practice.py
+++ practice.py
@@ -1,12 +1,3 @@
-import time
-
-def countdown(seconds):
-    while seconds:
-        mins, secs = divmod(seconds, 60)
-        timer = '{:02d}:{:02d}'.format(mins, secs)
-        print(timer, end="\r")
-        time.sleep(1)
-        seconds -= 1
-    print("Time's up!")
-    
-countdown(60)
+import calendar
+year = int(input("Enter year: "))
+print(calendar.calendar(year))
